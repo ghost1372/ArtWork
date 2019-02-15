74875d3c8230feb21466f7368d83a7cb7dd6deaf
Mahdi Hosseini <mahdidvb72@gmail.com>
2019-02-15 21:33:38 +0330

    update database

--- ArtWork/Config/AppVar.cs
+++ ArtWork/Config/AppVar.cs
@@ -4,7 +4,7 @@ namespace ArtWork
 {
     public class AppVar
     {
-        public static readonly int NumberOfAllItemExist = 9300;
+        public static readonly int NumberOfAllItemExist = 9360;
         public static readonly string imagesBaseUrl = "https://kraken99.blob.core.windows.net/images4000xn/";
         public static readonly string jsonBaseUrl = "https://kraken99.blob.core.windows.net/tileinfo/";
 
