d7b8e9f356d7107569ae01c5eb0ddf92a0fde5ab
Mahdi Hosseini <mahdidvb72@gmail.com>
2023-10-17 21:13:32 +0330

    Update ArtWorkConfig.cs

--- dev/Common/ArtWorkConfig.cs
+++ dev/Common/ArtWorkConfig.cs
@@ -4,12 +4,12 @@ using Nucs.JsonSettings.Modulation;
 namespace ArtWork.Common;
 public class ArtWorkConfig : JsonSettings, IVersionable
 {
-    [EnforcedVersion("2.3.0.0")]
-    public virtual Version Version { get; set; } = new Version(2, 3, 0, 0);
+    [EnforcedVersion("2.3.1.0")]
+    public virtual Version Version { get; set; } = new Version(2, 3, 1, 0);
     public override string FileName { get; set; } = Constants.AppConfigPath;
 
     public virtual string ArtWorkDirectory { get; set; } = Constants.ArtWorkPath;
     public virtual int AvailableArtWorkCount { get; set; } = 10431;
-    public virtual bool IsShowNudes { get; set; } = true;
+    public virtual bool IsShowNudes { get; set; } = false;
     public virtual bool IsShowOnlyNudes { get; set; } = false;
 }
