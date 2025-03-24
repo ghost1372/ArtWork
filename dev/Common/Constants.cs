d6918e0021fac2dca82bbc17db2e10bfd1a7bf86
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-03-24 18:30:14 +0330

    Update Constants.cs

--- dev/Common/Constants.cs
+++ dev/Common/Constants.cs
@@ -6,8 +6,8 @@ public static class Constants
 
     public static readonly string AppName = ProcessInfoHelper.ProductNameAndVersion;
 
-    public static readonly string ArtWorkBaseUrl = "https://kraken99.blob.core.windows.net/images4000xn/";
-    public static readonly string JsonBaseUrl = "https://kraken99.blob.core.windows.net/tileinfo/";
+    public static readonly string ArtWorkBaseUrl = "https://kraken66.blob.core.windows.net/images4000xn/";
+    public static readonly string JsonBaseUrl = "https://kraken66.blob.core.windows.net/tileinfo/";
 
     private static readonly string ArtWorkFolderName = "ArtWork";
     public static readonly string ArtWorkPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), AppName, ArtWorkFolderName);
