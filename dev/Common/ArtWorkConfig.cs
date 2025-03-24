74fe0cfc5db9ebc8d2db13db9c53e24ed43ea6d7
Mahdi Hosseini <mahdidvb72@gmail.com>
2025-03-24 18:30:17 +0330

    Update ArtWorkConfig.cs

--- dev/Common/ArtWorkConfig.cs
+++ dev/Common/ArtWorkConfig.cs
@@ -6,12 +6,12 @@ namespace ArtWork.Common;
 [GenerateAutoSaveOnChange]
 public partial class ArtWorkConfig : NotifiyingJsonSettings, IVersionable
 {
-    [EnforcedVersion("2.3.1.0")]
-    public Version Version { get; set; } = new Version(2, 3, 1, 0);
+    [EnforcedVersion("3.0.0.0")]
+    public Version Version { get; set; } = new Version(3, 0, 0, 0);
     public string fileName { get; set; } = Constants.AppConfigPath;
 
     public string artWorkDirectory { get; set; } = Constants.ArtWorkPath;
-    public int availableArtWorkCount { get; set; } = 10431;
+    public int availableArtWorkCount { get; set; } = 10461;
     public bool isShowNudes { get; set; } = false;
     public bool isShowOnlyNudes { get; set; } = false;
 }
